d1c0ddd7b77e5ea26600e8afe71510f25cfc8235
edogola4 <edogola4@gmail.com>
2024-10-23 21:05:56 +0300

    done

--- password/passgen/views.py
+++ password/passgen/views.py
@@ -8,7 +8,15 @@ def home(request):
 
 def passgen(request):
     char = list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get("uppercase"):
+        char.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get("digits"):
+        char.extend(list('0123456789'))
+    if request.GET.get("symbols"):
+        char.extend(list('!@#$%^&*()_+'))
+
+    length = int(request.GET.get("length", 15))
     password = ""
-    for x in range(15):
+    for x in range(length):
         password += random.choice(char)
     return render(request, 'password.html', {'password': password})
